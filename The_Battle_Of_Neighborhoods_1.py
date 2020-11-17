167758caef8ac710fe7f5e1ac77a34bb47242725
FaujaMastaan <66206324+FaujaMastaan@users.noreply.github.com>
2020-11-17 17:22:11 +0530

    Add files via upload

---
+++ The_Battle_Of_Neighborhoods_1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # The Battle of Neighborhoods 
+# ##### This project is part of my IBM Data Science specialization where we will be comparing Neighborhoods of City of Toronto i.e. amenities present in the nieghborhood and whether it is same as other neighborhoods 
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
